a9130f61c2c4df4c6a788434442083141e09f736
Bright Ihegworo <brightihegworo@Brights-MacBook-Air.local>
2023-02-10 09:11:46 -0600

    homework 1

---
+++ lesson_1/__init__.py
@@ -0,0 +1,6 @@
+
+y = 9
+x = 4
+z = 7.878899
+v = int(z)
+print(v, type(v))
---
+++ lesson_2/__init__.py
@@ -0,0 +1,9 @@
+#my_name = input("Enter you name:")
+#print(f"Hi {my_name}")
+
+str_1 = 'Hello\\nworld'
+str_2 = ' World!'
+#print(str_1)
+#print(7//2)
+
+print(bool(0))
